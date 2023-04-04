8b2b3f73ce003173833f1a8756f95f4d8d0554d9
Temi Sam <temitopesam5@gmail.com>
2023-04-04 18:47:31 +0100

    fix: fix import format in 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 '''expressing coroutine using asyncio and time module
 '''
-import asyncio, time
+import asyncio
+import time
 import random
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
