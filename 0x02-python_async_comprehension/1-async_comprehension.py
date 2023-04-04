b330786c6096e7a2f05e6e35eaae4304e673020a
Temi Sam <temitopesam5@gmail.com>
2023-04-04 18:38:55 +0100

    fix: fix a bug

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,5 +9,4 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> typing.List[float]:
   '''The coroutine collect random numbers and returns them
   '''
-  result = [i async for i in async_generator()]
-    return result
+  return [i async for i in async_generator()]
